7c36ccbbc04018b658797d9f269ce0a6a151aba5
Khusheemaru <marukhushee@gmail.com>
2026-05-27 17:28:51 +0530

    fix(vercel): output build artifacts to root dist folder

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envDir: '../../',
+  build: {
+    outDir: '../../dist',
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/supabase-proxy': {
